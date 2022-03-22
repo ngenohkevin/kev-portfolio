75b513ba17ee529db6404e0773e5710b2dc15ece
ngenohkevin <ngenohkevin19@gmail.com>
2022-03-22 16:50:50 +0300

    first commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import Go from '../assets/go.png';
+import Docker from '../assets/docker.png';
+import ReactImg from '../assets/react.png';
+import Kubernetes from '../assets/kubernetes.png';
+import linux from '../assets/linux.png';
+import Postgresql from '../assets/postgresql.png';
+import GitHub from '../assets/github.png';
+
+const Skills = () => {
+    return (
+        <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+                    <p className='py-4'> // These are the technologies I've worked with</p>
+                </div>
+
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Go} alt="HTML icon" />
+                        <p className='my-4'>GoLang</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
+                        <p className='my-4'>React</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Docker} alt="HTML icon" />
+                        <p className='my-4'>Docker</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Kubernetes} alt="HTML icon" />
+                        <p className='my-4'>Kubernetes</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
+                        <p className='my-4'>GITHUB</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={linux} alt="HTML icon" />
+                        <p className='my-4'>Linux</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Postgresql} alt="HTML icon" />
+                        <p className='my-4'>PostgreSQL</p>
+                    </div>
+                    {/*<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>*/}
+                    {/*    <img className='w-20 mx-auto' src={Go} alt="HTML icon" />*/}
+                    {/*    <p className='my-4'>Go</p>*/}
+                    {/*</div>*/}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
